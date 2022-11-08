96bca08f17bf822db9014f12592dea2172715f24
HHM-1999 <hhm639069@gmail.com>
2022-11-09 03:05:33 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,6 +25,38 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 //   // perform actions on the collection object
 //   client.close();
 // });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('lunchzone').collection('services');
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+
+        })
+
+        app.get('/service', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const service = await cursor.limit(3).toArray();
+            res.send(service);
+
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
 
 
 
